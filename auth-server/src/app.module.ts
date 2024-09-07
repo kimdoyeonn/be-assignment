b393b5437f5349bf63cb776fb3749e92fe807c74
Doyeon Kim <doyeonkim.yeon@gmail.com>
2024-09-07 14:54:15 +0900

    :hammer: chore: DB 세팅 및 엔티티 설계 #3

--- auth-server/src/app.module.ts
+++ auth-server/src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entity/user.entity';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '3306'),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      autoLoadEntities: true,
+      logging: true,
+      namingStrategy: new SnakeNamingStrategy(),
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ auth-server/src/entity/user.entity.ts
@@ -0,0 +1,14 @@
+import { Column, Entity } from 'typeorm';
+import { BaseEntity } from './base.entity';
+
+@Entity()
+export class User extends BaseEntity {
+  @Column({ type: 'varchar', length: 128, unique: true })
+  username: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  password: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  refreshToken: string;
+}
